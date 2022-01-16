0a9ba9e38b199036b6f40dbffdec9822f02e309f
Irina Timoshenko <irene.timoshenko@yandex.ru>
2022-01-16 11:00:53 +0300

    hotfix: bem, semantics, styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,37 @@
-let popup = document.querySelector('.popup');
-let profileEditButton = document.querySelector('.profile__edit-button'); 
-let popupCloseButton = document.querySelector('.popup__close');
-let nameInput = document.querySelector('.popup__input-name');
-let descriptionInput = document.querySelector('.popup__input-description');
-let profileName = document.querySelector('.profile__title');
-let profileDescription = document.querySelector('.profile__subtitle');
-let formElement = document.querySelector('.popup__container');
+let popup = document.querySelector(".popup");
+let profileEditButton = document.querySelector(".profile__edit-button");
+let popupCloseButton = document.querySelector(".popup__close");
+let nameInput = document.querySelector(".popup__input_type_name");
+let descriptionInput = document.querySelector(".popup__input_type_description");
+let profileName = document.querySelector(".profile__title");
+let profileDescription = document.querySelector(".profile__subtitle");
+let formElement = document.querySelector(".popup__container");
 
-profileEditButton.addEventListener('click', () => {
-  popup.classList.add('popup_opened');
-  nameInput.setAttribute('value', profileName.textContent);
-  descriptionInput.setAttribute('value', profileDescription.textContent);
-})
-
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function openPopup() {
+  popup.classList.add("popup_opened");
+  nameInput.setAttribute("value", profileName.textContent);
+  descriptionInput.setAttribute("value", profileDescription.textContent);
 }
 
-popupCloseButton.addEventListener('click', closePopup);
+profileEditButton.addEventListener("click", openPopup);
 
-popup.addEventListener('click', (event) => {
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+popupCloseButton.addEventListener("click", closePopup);
+
+popup.addEventListener("click", (event) => {
   if (event.target === event.currentTarget) {
     closePopup();
   }
-})
+});
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileDescription.textContent = descriptionInput.value;
   closePopup();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener("submit", formSubmitHandler);

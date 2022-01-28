1197944567c4c49811fcddaa74d726c4d61e4005
Irina Timoshenko <irene.timoshenko@yandex.ru>
2022-01-28 16:00:55 +0300

    refactor: code optimization

--- scripts/index.js
+++ scripts/index.js
@@ -30,52 +30,27 @@ const photoViewPopup = document.querySelector(".popup-photo-view");
 const photoViewCloseButton = document.querySelector(
   ".popup-photo-view__close-button"
 );
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
+
+//Open popup
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
 
 //Open popup (profile edit)
 function openPopupProfile() {
-  popupProfile.classList.add("popup_opened");
+  openPopup(popupProfile);
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
 }
 
-profileEditButton.addEventListener("click", openPopupProfile);
-
 //Open popup (card add)
 function openPopupAddCard() {
-  popupAddCard.classList.add("popup_opened");
+  openPopup(popupAddCard);
 }
 
-addCardButton.addEventListener("click", openPopupAddCard);
-
 //Popups close
-function closePopup() {
-  document.querySelector(".popup_opened").classList.remove("popup_opened");
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
 }
 
 function closePopupClickOutside(popup) {
@@ -85,23 +60,25 @@ function closePopupClickOutside(popup) {
   }
   popup.addEventListener("click", popupClose);
 }
+function closePopupProfile() {
+  closePopup(popupProfile);
+}
 
-popupProfileCloseButton.addEventListener("click", closePopup);
-popupAddCardCloseButton.addEventListener("click", closePopup);
-photoViewCloseButton.addEventListener("click", closePopup);
-closePopupClickOutside(popupProfile);
-closePopupClickOutside(popupAddCard);
-closePopupClickOutside(photoViewPopup);
+function closePopupAddCard() {
+  closePopup(popupAddCard);
+}
+
+function closePopupPhotoView() {
+  closePopup(photoViewPopup);
+}
 
 //Submit button click (profile edit)
 function formSubmitHandlerProfileEdit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileDescription.textContent = descriptionInput.value;
-  closePopup(evt);
+  closePopup(popupProfile);
 }
-
-formProfileEdit.addEventListener("submit", formSubmitHandlerProfileEdit);
 
 //Add new card
 function addCard(link, title) {
@@ -119,65 +96,57 @@ function addCard(link, title) {
     newCardImage.src = "./images/no-image.jpg";
   };
 
+  newCard
+    .querySelector(".card__like-button")
+    .addEventListener("click", clickLike);
+  newCard
+    .querySelector(".card__trash-button")
+    .addEventListener("click", removeCard);
+  newCardImage.addEventListener("click", сlickImage);
   sectionWithCards.prepend(newCard);
-  addLikeClickListener(newCard.querySelector(".card__like-button"));
-  addTrashClickListener(newCard.querySelector(".card__trash-button"));
-  addImageClickListener(newCardImage);
 }
 
 //Submit button click (add card)
 function submitCard(evt) {
   evt.preventDefault();
   addCard(cardImageLinkInput.value, cardTitleInput.value);
-  closePopup(evt);
+  closePopup(popupAddCard);
   cardTitleInput.value = "";
   cardImageLinkInput.value = "";
 }
 
-formAddCard.addEventListener("submit", submitCard);
-
 //Like button click
-function addLikeClickListener(el) {
-  function clickLike() {
-    el.classList.toggle("card__like-button_active");
-  }
-  el.addEventListener("click", clickLike);
+function clickLike(evt) {
+  evt.target.classList.toggle("card__like-button_active");
 }
-
-cardLikeButtonsList.forEach((element) => {
-  addLikeClickListener(element);
-});
 
 //Trash button click: delete card
-function addTrashClickListener(el) {
-  function removeCard() {
-    el.parentElement.remove();
-  }
-  el.addEventListener("click", removeCard);
+function removeCard(evt) {
+  evt.target.parentElement.remove();
 }
 
-cardTrashButtonsList.forEach((element) => {
-  addTrashClickListener(element);
-});
+//Card image click
+function сlickImage(evt) {
+  openPopup(photoViewPopup);
+  document.querySelector(".popup-photo-view__image").src = evt.target.src;
+  document.querySelector(".popup-photo-view__title").textContent =
+    evt.target.parentElement.querySelector(".card__title").textContent;
+}
+
+//Event listeners
+profileEditButton.addEventListener("click", openPopupProfile);
+addCardButton.addEventListener("click", openPopupAddCard);
+popupProfileCloseButton.addEventListener("click", closePopupProfile);
+popupAddCardCloseButton.addEventListener("click", closePopupAddCard);
+photoViewCloseButton.addEventListener("click", closePopupPhotoView);
+formProfileEdit.addEventListener("submit", formSubmitHandlerProfileEdit);
+formAddCard.addEventListener("submit", submitCard);
+
+closePopupClickOutside(popupProfile);
+closePopupClickOutside(popupAddCard);
+closePopupClickOutside(photoViewPopup);
 
 //Add 6 default cards
 initialCards.forEach((card) => {
   addCard(card.link, card.name);
-});
-
-//Card image click
-const cardsImagesList = document.querySelectorAll(".card__image");
-
-function addImageClickListener(img) {
-  function сlickImage() {
-    photoViewPopup.classList.add("popup_opened");
-    document.querySelector(".popup-photo-view__image").src = img.src;
-    document.querySelector(".popup-photo-view__title").textContent =
-      img.parentElement.querySelector(".card__title").textContent;
-  }
-  img.addEventListener("click", сlickImage);
-}
-
-cardsImagesList.forEach((image) => {
-  addImageClickListener(image);
 });
